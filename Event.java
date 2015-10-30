30a49ad1bd6c0b98a8216e3cdddc9aff62479f0c
Sam Hanna <samanthahanna15@gwmail.gwu.edu>
2015-10-30 12:13:38 -0400

    Changes to EventEvent.java

--- Event.java
+++ Event.java
@@ -1,7 +1,21 @@
 public class Event{
    
+   public static final int INTERSECTION = 0;
+   public static final int TURN = 1;
+   public static final int MOVEMENT = 2;
+   public static final int WALL = 6;
+   public static final int FAILED = 7;
+   public static final int UNCHECKED = 8;
+   public static final int CHECK_IN_PROGRESS = 9;
+   
+   public int leftStatus;
+   public int rightStatus;
+   public int forwardStatus;
+
+
    public Event(){
    }
+   
    
    //intersections
       //status left right forward
