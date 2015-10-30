a74dcc1edc3855992459e6445bd3d04a04134104
Sam Hanna <samanthahanna15@gwmail.gwu.edu>
2015-10-30 12:00:07 -0400

    comments on Event

---
+++ Event.java
@@ -0,0 +1,13 @@
+public class Event{
+   
+   public Event(){
+   }
+   
+   //intersections
+      //status left right forward
+         //wall, failed route, unchecked, check in progress
+   //turns
+      //direction
+   //movements
+      //distance
+}
