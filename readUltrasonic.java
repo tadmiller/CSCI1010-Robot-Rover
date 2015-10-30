5ca9f7218f277893603e43ce4cc4ec9f4c8ffd7b
Nathan Walker <me@nwalker.org>
2015-10-30 11:15:29 -0400

    Fixed compiler errors for readUltrasonic

--- readUltrasonic.java
+++ readUltrasonic.java
@@ -1,18 +1,19 @@
 public class readUltrasonic{
 
 
-public static void main(String [] args){
-      while (Button.readButtons() != Button.ID_ESCAPE)
-		{
-      LCD.clear();
-      LCD.drawInt(sonic.getDistance(), 0, 3);
-	  //Button.waitForPress();
-	  try {
-		Thread.sleep(250);
-	  } catch (Exception e) {}
-    }
-		}
-	
-		System.out.println("Done");
-   }
-   }
+	public static void main(String [] args){
+	      while (Button.readButtons() != Button.ID_ESCAPE)
+			{
+	      		LCD.clear();
+	      		LCD.drawInt(sonic.getDistance(), 0, 3);
+		 		 //Button.waitForPress();
+		  		try {
+					Thread.sleep(250);
+		  		} catch (Exception e) {
+			  		
+		  		}
+			}
+		
+			System.out.println("Done");
+	}
+}
