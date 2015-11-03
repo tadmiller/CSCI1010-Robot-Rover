624d85644a98cf5f434ac7adfe23186452d39565
Nathan Walker <me@nwalker.org>
2015-11-02 19:18:15 -0500

    Added an outline for the MazeRunner program

---
+++ MazeRunner.java
@@ -0,0 +1,69 @@
+import java.util.Stack;
+
+public class MazeRunner {
+	
+	Stack stack;
+	
+	public MazeRunner() {
+		stack = new Stack();
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void moveForwardUntilStopped() {
+		// Create a movement event
+		// Keep a log of distance moved
+		
+		// End scenarios:
+		// If wall, call reverseToLast()
+		// If intersection, call processNewIntersection()
+		// If finish point, call reverseAll()
+	}
+	
+	public void processNewIntersection() {
+		// Read ultrasonic sensor to get whether or not
+		// there is a wall on the left side
+		
+		// Call decideNextAction() 
+	}
+	
+	public void decideNextAction() {
+		// Decide what to do next
+		
+		// If stack is empty, call moveForwardUntilStopped()
+		
+		// If at an intersection, determine which paths have not
+		// been checked and test them.
+		
+		// If at an intersection and all options have been tried,
+		// call reverseToLast()
+		
+		// If none of the above, throw exception
+	}
+	
+	public void reverseToLast() {
+		// Reverse the stack so that you arrive at the last intersection
+		
+		// Use movement data to estimate where it is going to be
+		
+		// Don't forget to turn around at the beginning and end
+		
+		// Then, call decideNextAction()
+	}
+	
+	public void reverseAll() {
+		// Reverse the stack to return to the start
+	}
+	
+	public void makeTurn(int direction) {
+		// Make a turn in the given direction
+		
+		// Use direction types defined in Event and
+		// RobotRover functions
+		
+		// Add a turn event to the stack
+	}
+	
+}
