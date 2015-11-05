831b08670ea4f28b889c47fdb2702ee48ec8c23f
Tad Miller <tadmiller@gwu.edu>
2015-11-05 18:29:19 -0500

    Fixed compilation issues

--- RobotRover.java
+++ RobotRover.java
@@ -106,18 +106,12 @@ public class RobotRover
 	
 	// Follow a line with color sensor until a color is hit
 	// Returns a result object (see below)
-<<<<<<< HEAD
 	public MovementResult followLineUntilStopped(double maxDistance)
 	{
 		ColorSensor colorSensor = new ColorSensor(SensorPort.S1);
 		TouchSensor touchSensor = new TouchSensor(SensorPort.S2);
-=======
-	public MovementResult followLineUntilStopped() {
-		ColorSensor colorSensor = new ColorSensor(SensorPort.S2);
-		TouchSensor touchSensor = new TouchSensor(SensorPort.S1);
 		
 		colorSensor.setFloodlight(true);
->>>>>>> origin/master
 		
 		Motor.B.setSpeed(200);
 		Motor.C.setSpeed(200);
