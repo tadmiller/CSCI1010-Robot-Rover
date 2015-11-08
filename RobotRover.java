4bada738cec00f39906c4d8062174757a5f39f8d
Nathan Walker <me@nwalker.org>
2015-11-08 12:07:17 -0500

    Removed much of the old code from RobotRover

--- RobotRover.java
+++ RobotRover.java
@@ -18,51 +18,6 @@ public class RobotRover
 	{
 	}
 
-	// Drive forward until we hit something, back up, 
-	// turn left, drive forward again, then turn right and repeat.
-	public void avoidObstacles()
-	{
-		// End program by pressing two middle buttons together
-		while (Button.readButtons() != Button.ID_ESCAPE)
-		{
-			moveForwardUntilPressed();
-			
-			sleep(1);
-
-			moveBackward(1);
-
-			turnLeft();
-
-			moveForward(1);
-
-			turnRight();
-		}
-
-		System.out.println("Done");
-	}
-	
-	// Same as avoidObstacles(), with ultrasonic sensor instead of touch
-	public void avoidObstaclesUltrasonic()
-	{
-		// End program by pressing two middle buttons together
-		while (Button.readButtons() != Button.ID_ESCAPE)
-		{
-			moveForwardUntilNearObject();
-			
-			sleep(1);
-	
-			moveBackward(1);
-	
-			turnLeft();
-	
-			moveForward(1);
-	
-			turnRight();
-		}
-	
-		System.out.println("Done");
-	}
-
 	/**
 	 * Has the robot pause/wait for a given amount of time
 	 * 
@@ -102,30 +57,6 @@ public class RobotRover
 		
 		return 0; // error/no adjustment needed
 	}
-
-	// Follow a black line with input from light sensor
-	public void followLine()
-	{
-		LightSensor t1 = new LightSensor(SensorPort.S1);
-		Motor.B.setSpeed(200);
-		Motor.C.setSpeed(200);
-		
-		// End program by pressing two middle buttons together
-		while (Button.readButtons() != Button.ID_ESCAPE)
-		{
-			while (t1.readValue() >= 24)
-				Motor.B.forward();
-
-			Motor.B.stop(true);
-
-			while (t1.readValue() < 24)
-				Motor.C.forward();
-
-			Motor.C.stop(true);
-		}
-
-		//System.out.println("Done");
-	}
 	
 	// Follow a line with color sensor until a color is hit
 	// Returns a result object (see below)
@@ -149,8 +80,6 @@ public class RobotRover
 		// End program by pressing two middle buttons together
 		while (distanceTravelled < (maxDistance - 1) || maxDistance == -1)
 		{
-         
-                
 
 			//System.out.println("running");
 			if (touchSensor.isPressed())
@@ -192,32 +121,6 @@ public class RobotRover
 		
 		return new MovementResult(isEndpoint, isWall, distanceTravelled);
 	}
-   
-   public void followLineForReal(){
-    ColorSensor colorSensor = new ColorSensor(SensorPort.S1);
-		Motor.B.setSpeed(200);
-		Motor.C.setSpeed(200);
-		
-      ColorSensor.Color color = colorSensor.getColor();
-			
-		double hue = getColorSensorH();
-		// End program by pressing two middle buttons together
-		while (Button.readButtons() != Button.ID_ESCAPE)
-		{
-
-      while (hue > 80 || hue < 60)
-				Motor.B.forward();
-
-			Motor.B.stop(true);
-
-			while (hue > 60 && hue < 80 )
-				Motor.C.forward();
-
-			Motor.C.stop(true);
-       }
-
-   }
-   
 
 	// black is 16 <=
 	// white is > 290, < 310
@@ -268,35 +171,6 @@ public class RobotRover
 			hsv[0] +=360;
 		
 		return hsv;
-	}
-
-	// Drive forward until we hit something.
-	public void moveForwardUntilPressed()
-	{
-		TouchSensor t2 = new TouchSensor(SensorPort.S2);
-		TouchSensor t3 = new TouchSensor(SensorPort.S3);
-
-		moveForward(-1);
-
-		while (!t2.isPressed() && !t3.isPressed())
-			Thread.yield();
-
-		stop();
-	}
-	
-	// Drive forward until an object gets close
-	public void moveForwardUntilNearObject()
-	{
-		
-		UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S4);
-	
-		moveForward(-1);
-		
-		// While distance between nearest object and sensor is greater than 30 cm
-		while (sonic.getDistance() > 30)
-			Thread.yield();
-	
-		stop();
 	}
 
 	// Move forward for a specified amount of time (or infinite).
